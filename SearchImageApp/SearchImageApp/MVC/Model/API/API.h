25c808686350e21bbf4b7e958a4f6d2bff8ebea4
y4gamilight <ltthanh36711@gmail.com>
2017-09-21 18:29:36 +0700

    - restructure folder - add library check network

---
+++ SearchImageApp/SearchImageApp/MVC/Model/API/API.h
@@ -0,0 +1,13 @@
+//
+//  API.h
+//  SearchImageApp
+//
+//  Created by DELL7447 on 9/21/17.
+//  Copyright © 2017 y4gamilight. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface API : NSObject
+
+@end
---
+++ SearchImageApp/SearchImageApp/MVC/Model/API/API.m
@@ -0,0 +1,13 @@
+//
+//  API.m
+//  SearchImageApp
+//
+//  Created by DELL7447 on 9/21/17.
+//  Copyright © 2017 y4gamilight. All rights reserved.
+//
+
+#import "API.h"
+
+@implementation API
+
+@end
